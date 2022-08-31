bd75f2ecbdae1475121700ab00db840813532f2f
Duruaku Micheal <duruakuebuka@gmail.com>
2022-08-31 15:03:14 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float counter = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && counter > 1)
+        {
+               
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            counter = 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Pet-Challenge
+ A C# game challenge where the farmer sends his dog to pick up the ball
